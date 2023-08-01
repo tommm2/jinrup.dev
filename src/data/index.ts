0c2608b8f0f255344c293e80945209797f85767a
tomjin871114 <tom875694231@gmail.com>
2023-08-01 23:47:15 +0800

    feature: Add some useful static data

---
+++ src/data/index.ts
@@ -0,0 +1,64 @@
+const navLinks = [
+	{
+		title: '部落格',
+		href: '/blog',
+	},
+	{
+		title: '作品集',
+		href: '/projects',
+	},
+	{
+		title: '關於',
+		href: '/about',
+	},
+];
+
+const techStacks = [
+	{
+		category: '框架',
+		link: {
+			title: 'Next.js',
+			href: 'https://nextjs.org/',
+		},
+	},
+	{
+		category: 'UI 樣式',
+		link: {
+			title: 'Tailwindcss',
+			href: 'https://tailwindcss.com/',
+		},
+	},
+	{
+		category: '部署',
+		link: {
+			title: 'Vercel',
+			href: 'https://vercel.com/',
+		},
+	},
+	{
+		category: '內容管理',
+		link: {
+			title: 'Contentlayer',
+			href: 'https://www.contentlayer.dev/',
+		},
+	},
+];
+
+const experiences = [
+	{
+		title: '擔任小熊科技的前端工程師',
+		period: '2022/05 - 現在',
+		isProgressing: true,
+	},
+	{
+		title: '畢業於銘傳大學 - 電腦與通訊工程學系',
+		period: '2017/09 - 2021/06',
+		isProgressing: false,
+	},
+];
+
+export {
+	navLinks,
+	techStacks,
+	experiences,
+};
