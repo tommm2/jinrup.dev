6abbee4065a49d3912de9eb1179416be3ad6921d
tomjin871114 <--global>
2023-08-23 23:39:14 +0800

    chore: update contentlayer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,13 +1,7 @@
 import { ComputedFields, defineDocumentType, makeSource } from 'contentlayer/source-files';
-import remarkGfm from 'remark-gfm';
-import rehypeSlug from 'rehype-slug';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import { visit } from 'unist-util-visit';
 
-function convertToSlug(context: string) {
-	return context.replace(/\.mdx$/, '');
-}
+import { convertToSlug } from '@/lib/utils';
+import { mdxConfigs } from '@/data';
 
 const computedFields: ComputedFields = {
 	slug: {
@@ -21,7 +15,6 @@ const computedFields: ComputedFields = {
 			const parentPath = doc._raw.sourceFileDir;
 
 			return `/${parentPath}/${slug}`;
-
 		},
 	},
 };
@@ -60,69 +53,5 @@ export const Page = defineDocumentType(() => ({
 export default makeSource({
 	contentDirPath: 'src/content',
 	documentTypes: [Post, Project, Page],
-	mdx: {
-		esbuildOptions: (opts) => {
-			opts.tsconfig = `${process.env.PWD}/tsconfig.mdx.json`;
-
-			return opts;
-		},
-		remarkPlugins: [remarkGfm],
-		rehypePlugins: [
-			() => (tree) => {
-				visit(tree, (node) => {
-				  if (node?.type === 'element' && node?.tagName === 'pre') {
-						const [codeEl] = node.children;
-
-						if (codeEl.tagName !== 'code') return;
-
-						node.raw = codeEl.children?.[0].value;
-				  }
-				});
-			},
-			rehypeSlug,
-			[
-				rehypePrettyCode,
-				{
-					theme: 'one-dark-pro',
-					onVisitLine(node: any) {
-						// Prevent lines from collapsing in `display: grid` mode, and allow empty
-						if (node.children.length === 0) {
-							node.children = [{ type: 'text', value: ' ' }];
-						}
-					},
-					onVisitHighlightedLine(node: any) {
-						node.properties.className.push('line--highlighted');
-					},
-					onVisitHighlightedWord(node: any) {
-						node.properties.className = ['word--highlighted'];
-					},
-				},
-			],
-			[
-				rehypeAutolinkHeadings,
-				{
-					properties: {
-						className: ['anchor'],
-						ariaLabel: 'Title anchor',
-					},
-				},
-			],
-			// reference: https://claritydev.net/blog/copy-to-clipboard-button-nextjs-mdx-rehype
-			() => (tree) => {
-				visit(tree, (node) => {
-					if (node?.type === 'element' && node?.tagName === 'div') {
-						if (!('data-rehype-pretty-code-fragment' in node.properties)) {
-							return;
-						}
-
-						for (const child of node.children) {
-							if (child.tagName === 'pre') {
-								child.properties['raw'] = node.raw;
-							}
-						}
-					}
-				});
-			},
-		],
-	},
+	mdx: mdxConfigs,
 });
