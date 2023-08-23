04447c8d8ee4455ce5149ce8a0f4f90a6ac9ad4c
tomjin871114 <--global>
2023-08-23 23:44:43 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 const configs =  {
 	darkMode: 'class',
 	content: [
@@ -14,12 +13,13 @@ const configs =  {
 			'lg': '1024px',
 			'xl': '1280px',
 		},
-		fontFamily: {
-			'sans': [
-				'var(--font-space)',
-				'var(--font-noto)',
-				...defaultTheme.fontFamily.sans,
-			],
+		colors: {
+			'primary': colors.amber,
+			'secondary': colors.rose,
+			'base': colors.zinc,
+			'emerald': colors.emerald,
+			'currentColor': 'currentColor',
+			'transparent': 'transparent',
 		},
 		fontWeight: {
 			'light': '300',
@@ -27,51 +27,28 @@ const configs =  {
 			'medium': '500',
 			'bold': '700',
 		},
-		colors: {
-			primary: {
-				'50': '#eef2ff',
-				'100': '#e0e7ff',
-				'200': '#c7d2fe',
-				'300': '#a5b4fc',
-				'400': '#818cf8',
-				'500': '#6366f1',
-				'600': '#4f46e5',
-				'700': '#4338ca',
-				'800': '#3730a3',
-				'900': '#312e81',
-				'950': '#1e1b4b',
-			},
-			secondary: {
-				'50': '#f0fdfa',
-				'100': '#ccfbf1',
-				'200': '#99f6e4',
-				'300': '#5eead4',
-				'400': '#2dd4bf',
-				'500': '#14b8a6',
-				'600': '#0d9488',
-				'700': '#0f766e',
-				'800': '#115e59',
-				'900': '#134e4a',
-				'950': '#042f2e',
-			},
-			base: {
-				'50': '#fafafa',
-				'100': '#f4f4f5',
-				'200': '#e4e4e7',
-				'300': '#d4d4d8',
-				'400': '#a1a1aa',
-				'500': '#71717a',
-				'600': '#52525b',
-				'700': '#3f3f46',
-				'800': '#27272a',
-				'900': '#18181b',
-				'950': '#131316',
-			},
-			transparent: 'transparent',
+		fontFamily: {
+			'body': ['var(--font-barlow)', 'var(--font-noto)'],
 		},
 		extend: {
 			boxShadow: {
 				'card': '0px 2px 20px rgba(129, 140, 248, 0.2)',
+			},
+			animation: {
+				'wave': 'wave 6s linear infinite',
+			},
+			keyframes: {
+				'wave': {
+					'0%': {
+						'transform': 'translate(-50%, 0) rotateZ(0deg)',
+					},
+					'50%': {
+						'transform': 'translate(-50%, -2%) rotateZ(180deg)',
+					},
+					'100%': {
+						'transform': 'translate(-50%, 0%) rotateZ(360deg)',
+					},
+				},
 			},
 		},
 	},
