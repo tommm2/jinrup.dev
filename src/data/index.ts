20e971afdf81541b331dce837a0a6ea0720c8e06
tomjin871114 <--global>
2023-10-08 14:30:33 +0800

    rename nav-links

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
 import experiences from './experiences';
 import giscusConfigs from './giscus';
-import navLinks from './nav-links';
+import navLinks from './navLinks';
 import socials from './socials';
 
 export {
