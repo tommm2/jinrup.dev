1ffd7d90bb1fb9d04de91c8bf99fe0863635762a
tomjin871114 <--global>
2023-10-14 02:40:47 +0800

    update navLinks

--- src/data/navLinks.tsx
+++ src/data/navLinks.tsx
@@ -1,20 +1,15 @@
-import { RiFolder2Line, RiQuillPenLine, RiUser3Line } from 'react-icons/ri';
-
 const navLinks = [
 	{
 		title: 'blog',
 		href: '/blog',
-		icon: <RiQuillPenLine />,
 	},
 	{
 		title: 'projects',
 		href: '/projects',
-		icon: <RiFolder2Line />,
 	},
 	{
 		title: 'about',
 		href: '/about',
-		icon: <RiUser3Line />,
 	},
 ];
 
