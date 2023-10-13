b732f60a26fd16344dea580e728a46ec087e9a86
tomjin871114 <--global>
2023-10-14 02:39:39 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,27 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-const configs =  {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{js,ts,jsx,tsx,mdx}',
-	],
+const configs = {
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
-		screens: {
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-		},
 		fontWeight: {
-			'light': '300',
-			'normal': '400',
-			'medium': '500',
-			'bold': '700',
+			light: '300',
+			normal: '400',
+			medium: '500',
+			bold: '700',
 		},
 		fontFamily: {
-			'body': ['var(--font-barlow)', 'var(--font-noto)'],
+			sans: [
+				'var(--font-sans)',
+				...defaultTheme.fontFamily.sans,
+			],
 		},
 		extend: {
 			colors: {
-				'primary': colors.emerald,
-				'secondary': colors.blue,
-				'base': colors.neutral,
-			},
-			boxShadow: {
-				'card': '0px 2px 20px rgba(168, 85, 247, 0.2)',
-			},
-			animation: {
-				'wave': 'wave 6s linear infinite',
-			},
-			keyframes: {
-				'wave': {
-					'0%': {
-						'transform': 'translate(-50%, 0) rotateZ(0deg)',
-					},
-					'50%': {
-						'transform': 'translate(-50%, -2%) rotateZ(180deg)',
-					},
-					'100%': {
-						'transform': 'translate(-50%, 0%) rotateZ(360deg)',
-					},
-				},
+				primary: colors.emerald,
+				secondary: colors.blue,
+				base: colors.neutral,
 			},
 		},
 	},
