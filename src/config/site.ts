db94282192f0c37e79418bcca03918bf1d37d62b
tomjin871114 <--global>
2023-10-14 02:38:37 +0800

    add configs

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: 'NextUI - Beautiful, fast and modern React UI Library',
+	description: 'Make beautiful websites regardless of your design experience.',
+	ogImage: 'https://nextui.org/twitter-cards/nextui.jpeg',
+	author: 'Tom Jin',
+	email: 'tom875694231@gmail.com',
+	siteUrl: 'https://tomjin.vercel.app',
+	links: {
+		github: 'https://github.com/nextui-org/nextui',
+		linkedin: 'https://patreon.com/jrgarciadev',
+	},
+};
