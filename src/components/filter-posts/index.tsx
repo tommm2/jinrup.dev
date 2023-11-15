2f11020740093f1f28c220e7e555db854dc0cb63
tomjin871114 <--global>
2023-11-15 21:57:41 +0800

    update: move getGroupPostsWithYear to lib

--- src/components/filter-posts/index.tsx
+++ src/components/filter-posts/index.tsx
@@ -5,31 +5,7 @@ import { RiSearch2Line } from 'react-icons/ri';
 import { type Post } from 'contentlayer/generated';
 
 import PostCard from '@/components/post-card';
-import { groupBy } from '@/utils/helpers';
-
-function getGroupPostsWithYear(posts: Post[], query: string) {
-	if (!posts) return [];
-
-	const filteredPosts = !query
-		? posts
-		: posts.filter(({ title, description }) => {
-			const isTitleMatch = title.toLowerCase().includes(query.toLowerCase());
-			const isDescriptionMatch = description
-				.toLowerCase()
-				.includes(query.toLowerCase());
-
-			return isTitleMatch || isDescriptionMatch;
-		  });
-
-	const groups = groupBy(filteredPosts, (post) => post.year);
-
-	return Object.keys(groups)
-		.map((year) => ({
-			year: +year,
-			posts: groups[+year],
-		}))
-		.sort((a, b) => Number(b.year) - Number(a.year));
-}
+import { getGroupPostsWithYear } from '@/lib/contentlayer';
 
 type PostListProps = {
 	posts: Post[];
--- src/lib/contentlayer.ts
+++ src/lib/contentlayer.ts
@@ -1,3 +1,6 @@
+import { type Post } from 'contentlayer/generated';
+
+import { groupBy } from '@/utils/helpers';
 import { defaultLocale } from './navigation';
 
 type ItemWithSlugAndLanguage = {
@@ -20,4 +23,28 @@ export function getItemBySlugAndLocale<T extends ItemWithSlugAndLanguage>(
 	}
 
 	return itemsWithMatchSlug[index];
+}
+
+export function getGroupPostsWithYear(posts: Post[], query: string) {
+	if (!posts) return [];
+
+	const filteredPosts = !query
+		? posts
+		: posts.filter(({ title, description }) => {
+			const isTitleMatch = title.toLowerCase().includes(query.toLowerCase());
+			const isDescriptionMatch = description
+				.toLowerCase()
+				.includes(query.toLowerCase());
+
+			return isTitleMatch || isDescriptionMatch;
+		  });
+
+	const groups = groupBy(filteredPosts, (post) => post.year);
+
+	return Object.keys(groups)
+		.map((year) => ({
+			year: +year,
+			posts: groups[+year],
+		}))
+		.sort((a, b) => Number(b.year) - Number(a.year));
 }
