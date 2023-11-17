67daff2b3ad926c1be2597ba3d8892a891ccc6a2
tomjin871114 <--global>
2023-11-17 23:34:05 +0800

    feat: add loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+function Loading() {
+	return (
+		<span className='space-x-1'>
+			<span className='inline-flex animate-[loading_1.4s_ease-in-out_infinite] rounded-full'>
+				&bull;
+			</span>
+			<span className='inline-flex animate-[loading_1.4s_ease-in-out_0.2s_infinite] rounded-full'>
+				&bull;
+			</span>
+			<span className='inline-flex animate-[loading_1.4s_ease-in-out_0.4s_infinite] rounded-full'>
+				&bull;
+			</span>
+	  </span>
+	);
+}
+
+export default Loading;
