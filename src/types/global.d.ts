a88e6beb13922fe074f1f1d702285bfb4dd8992a
tomjin871114 <--global>
2023-11-28 22:29:29 +0800

    chore: add PinnedRepo type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,23 @@
-type Locale = 'zh-TW' | 'en' | undefined
+type Locale = 'zh-TW' | 'en' | undefined;
 
 type Views = {
-	slug: string
-	views: number
+	slug: string;
+	views: number;
+}
+
+type PinnedRepo = {
+	id: any;
+	name?: string;
+	url?: string;
+	stargazerCount?: number;
+	primaryLanguage?: {
+		name: string;
+		color: string;
+		id: string;
+	};
+	description?: string;
+	createdAt?: string;
+	forkCount?: number;
+	isArchived?: boolean;
+	isFork?: boolean;
 }
