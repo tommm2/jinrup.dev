ff5689453417148e2e532f85b2816c3b6794e725
tomjin871114 <--global>
2023-11-29 00:07:38 +0800

    fix: restore project list

--- src/components/project-list/index.tsx
+++ src/components/project-list/index.tsx
@@ -8,22 +8,21 @@ import { fetcher } from '@/lib/fetcher';
 
 function ProjectList() {
 	const {
-		data,
-	} = useSWR<Stats>('/api/github', fetcher);
+		data: projects,
+	} = useSWR<PinnedRepo[]>('/api/github', fetcher);
 
-	// if (!projects) {
-	// 	return <Loading className='mt-16 flex items-center justify-center' />;
-	// }
+	if (!projects) {
+		return <Loading className='mt-16 flex items-center justify-center' />;
+	}
 
 	return (
 		<div className='mt-16 grid animate-in grid-cols-1 gap-4 animation-delay-2 sm:grid-cols-2'>
-			stars: {data?.stars} - followers: {data?.followers}
-			{/* {projects.map((project) => (
+			{projects.map((project) => (
 				<ProjectCard
 					key={project.id}
 					project={project}
 				/>
-			))} */}
+			))}
 		</div>
 	);
 }
