bb0e1a22c7ddb01bd16058224412be3e8cb15cff
tomjin871114 <--global>
2023-12-03 00:48:48 +0800

    fix: change loading condition of project-list

--- src/components/project-list/index.tsx
+++ src/components/project-list/index.tsx
@@ -9,15 +9,16 @@ import { fetcher } from '@/lib/fetcher';
 function ProjectList() {
 	const {
 		data: projects,
+		isLoading,
 	} = useSWR<PinnedRepo[]>('/api/github?type=pinnedRepos', fetcher);
 
-	if (!projects) {
+	if (isLoading) {
 		return <Loading className='mt-16 flex items-center justify-center' />;
 	}
 
 	return (
 		<div className='mt-16 grid animate-in grid-cols-1 gap-4 animation-delay-2 sm:grid-cols-2'>
-			{projects.map((project) => (
+			{projects?.map((project) => (
 				<ProjectCard
 					key={project.id}
 					project={project}
