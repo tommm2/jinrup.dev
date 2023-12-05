fb18690905bda5c14e006337fa71c8271a9238d8
tomjin871114 <--global>
2023-12-06 00:25:13 +0800

    fix: add  format-string that depends on the locale

--- src/components/post-card/index.tsx
+++ src/components/post-card/index.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import Image from 'next/image';
 import type { Post } from 'contentlayer/generated';
+import { useLocale } from 'next-intl';
 
 import Link from '@/components/link';
 import ViewCounter from '@/components/view-counter';
 import { useEnabledFirstInView } from '@/hooks';
+import { defaultLocale } from '@/lib/navigation';
 import { formatDate } from '@/utils/date';
 
 type PostCardProps = {
@@ -13,42 +14,32 @@ type PostCardProps = {
 };
 
 function PostCard({ post }: PostCardProps) {
-	const { enabled, intersectionRef } = useEnabledFirstInView();
+	const locale = useLocale() as Locale;
+	const { enabled, intersectionRef }  = useEnabledFirstInView();
 
 	const {
 		slug,
 		title,
 		publishedAt,
-		image,
-		description,
 	} = post;
+
+	const formatString = locale === defaultLocale ? 'LLLd日' : 'LLLL d';
 
 	return (
 		<div ref={intersectionRef}>
 			<Link
-				className='-mx-4 flex flex-col gap-2 rounded-md p-4 font-medium transition-colors duration-300 hover:bg-base-800/40 sm:flex-row sm:gap-4'
+				className='opacity-hover font-medium'
 				href={`/blog/${slug}`}
 			>
-				<Image
-					className='hidden min-h-[6.25rem] w-full rounded-md object-cover object-center sm:block sm:max-w-[10rem]'
-					width={800}
-					height={600}
-					src={image}
-					alt={title}
-					priority
-				/>
-				<div className='space-y-1'>
-					<p className='font-bold'>{title}</p>
-					<p className='line-clamp-1 text-sm text-base-300/80 sm:line-clamp-2'>{description}</p>
-					<div className='flex text-sm text-base-300/60'>
-						<time dateTime={formatDate(publishedAt)}>
-							{formatDate(publishedAt)}
-						</time>
-						<span>．</span>
-						{enabled ? <ViewCounter slug={slug} /> : null}
-					</div>
-				</div>
+				{title}
 			</Link>
+			<div className='text-sm text-base-300/60'>
+				<time dateTime={publishedAt}>
+					{formatDate(publishedAt, formatString, locale)}
+				</time>
+				．
+				{enabled && <ViewCounter slug={slug} />}
+			</div>
 		</div>
 	);
 }
