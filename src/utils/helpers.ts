aef2b460945a8a22000a36046fa11e4072f0a29e
tomjin871114 <--global>
2023-12-06 00:27:00 +0800

    feat: add pick fn

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,13 @@ export function groupBy<T>(
 		return { ...prev, [groupKey]: group };
 	}, {});
 }
+
+export function pick<T, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> {
+	const ret: any = {};
+
+	keys.forEach(key => {
+	  ret[key] = obj[key];
+	});
+
+	return ret;
+}
