8ab4e6fb5412584c4e9483270b84085d09ff68ea
tomjin871114 <--global>
2023-12-05 17:01:52 +0800

    fix: update homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,101 +1,75 @@
 import Image from 'next/image';
-import { useLocale, useTranslations } from 'next-intl';
-import { allPosts } from 'contentlayer/generated';
-import { compareDesc } from 'date-fns';
+import { useTranslations } from 'next-intl';
 import {
-	RiArrowRightLine,
-	RiAtLine,
 	RiGithubFill,
 	RiLinkedinBoxFill,
+	RiMailSendFill,
+	RiPushpinLine,
 } from 'react-icons/ri';
 
-import GradientText from '@/components/gradient-text';
 import Link from '@/components/link';
-import PostCard from '@/components/post-card';
+import ProjectList from '@/components/project-list';
 import { siteConfig } from '@/config/site';
 
 function HomePage() {
-	const locale = useLocale();
 	const t = useTranslations();
-	const recentPosts = allPosts
-		.filter((post) => post.language === locale)
-		.sort((a, b) => compareDesc(new Date(a.publishedAt), new Date(b.publishedAt)))
-		.splice(0, 2);
 
 	return (
 		<>
-			<div className='space-y-8'>
-				<div className='flex flex-col gap-4 xs:flex-row xs:items-center'>
-					<Image
-						className='animate-in rounded-full bg-gradient-to-r from-base-200 to-base-200/60'
-						src='/images/avatar.png'
-						alt='avatar'
-						width={80}
-						height={80}
-						priority
-					/>
-					<div className='animate-in animation-delay-1'>
-						<GradientText
-							className='text-3xl font-bold tracking-tight'
-							as='h1'
-						>
-							Tom Jin.
-						</GradientText>
-						<p className='text-base-300/80'>{t('homePage.subTitle')}</p>
-					</div>
-				</div>
-				<div className='flex animate-in flex-wrap items-center gap-4 animation-delay-2'>
-					<p>{t('homePage.description')}</p>
-					<Link
-						className='inline-flex items-center gap-2 rounded-md bg-base-800 px-2 py-1.5 text-sm font-medium duration-300 hover:opacity-70'
-						aria-label='GitHub'
-						title='GitHub'
-						href={siteConfig.links.github}
-					>
-						<RiGithubFill className='h-6 w-6' />
-						<span className='hidden sm:inline'>Github</span>
-					</Link>
-					<Link
-						className='inline-flex items-center gap-2 rounded-md bg-blue-700/80 px-2 py-1.5 text-sm font-medium duration-300 hover:opacity-70'
-						aria-label='Linkedin'
-						title='Linkedin'
-						href={siteConfig.links.linkedin}
-					>
-						<RiLinkedinBoxFill className='h-6 w-6' />
-						<span className='hidden sm:inline'>Linkedin</span>
-					</Link>
-					<Link
-						className='inline-flex items-center gap-2 rounded-md bg-rose-600/80 px-2 py-1.5 text-sm font-medium duration-300 hover:opacity-70'
-						aria-label='Email'
-						title='Email'
-						href={`mailto:mail@${siteConfig.email}`}
-					>
-						<RiAtLine className='h-6 w-6' />
-						<span className='hidden sm:inline'>Email</span>
-					</Link>
+			<div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
+				<Image
+					className='animate-in rounded-full bg-base-200'
+					src='/images/avatar.png'
+					alt='avatar'
+					width={80}
+					height={80}
+					priority
+				/>
+				<div className='animate-in animation-delay-1'>
+					<h1 className='text-2xl font-bold tracking-tight text-base-200'>
+						Tom Jin
+					</h1>
+					<p className='text-base-200/70'>{t('homePage.subTitle')}</p>
 				</div>
 			</div>
-			<div className='mt-16 animate-in animation-delay-3'>
-				<div className='flex items-center justify-between'>
-					<GradientText className='text-xl font-bold tracking-tight'>
-						{t('common.latestPosts')}
-					</GradientText>
-					<Link
-						className='flex items-center gap-2 text-sm transition-opacity hover:opacity-70'
-						href='/blog'
-					>
-						{t('common.allPosts')}
-						<RiArrowRightLine />
-					</Link>
-				</div>
-				<div className='mt-4 flex flex-col gap-4'>
-					{recentPosts.map((post) => (
-						<PostCard
-							key={post.slug}
-							post={post}
-						/>
-					))}
-				</div>
+			<p className='mt-8 animate-in animation-delay-2'>
+				{t('homePage.description')}
+			</p>
+			<div className='mt-4 flex animate-in flex-wrap items-center gap-4 animation-delay-3'>
+				<Link
+					className='opacity-hover inline-flex items-center gap-1 font-medium text-base-300/80'
+					aria-label='GitHub'
+					title='GitHub'
+					href={siteConfig.links.github}
+				>
+					<RiGithubFill className='h-5 w-5' />
+					<span className='hidden sm:inline'>Github</span>
+				</Link>
+				<Link
+					className='opacity-hover inline-flex items-center gap-1 font-medium text-blue-500'
+					aria-label='Linkedin'
+					title='Linkedin'
+					href={siteConfig.links.linkedin}
+				>
+					<RiLinkedinBoxFill className='h-5 w-5' />
+					<span className='hidden sm:inline'>Linkedin</span>
+				</Link>
+				<Link
+					className='opacity-hover inline-flex items-center gap-1 font-medium text-rose-500'
+					aria-label='Email'
+					title='Email'
+					href={`mailto:mail@${siteConfig.email}`}
+				>
+					<RiMailSendFill className='h-5 w-5' />
+					<span className='hidden sm:inline'>Email</span>
+				</Link>
+			</div>
+			<div className='mt-16 animate-in space-y-4 animation-delay-4'>
+				<h2 className='flex items-center gap-2 tracking-tight text-base-200'>
+					<RiPushpinLine className='h-5 w-5' />
+					{t('common.pinned')}
+				</h2>
+				<ProjectList />
 			</div>
 		</>
 	);
