a7ae04f3b99bd2a7a47bf2f2b7b43f09b5f5aaac
tomjin871114 <--global>
2023-12-06 00:26:03 +0800

    fix: add coming soon text in project-list

--- src/components/project-list/index.tsx
+++ src/components/project-list/index.tsx
@@ -18,12 +18,14 @@ function ProjectList() {
 
 	return (
 		<div className='mt-16 grid animate-in grid-cols-1 gap-4 animation-delay-2 sm:grid-cols-2'>
-			{projects?.map((project) => (
-				<ProjectCard
-					key={project.id}
-					project={project}
-				/>
-			))}
+			{projects?.length !== 0 ? (
+				projects?.map((project) => (
+					<ProjectCard
+						key={project.id}
+						project={project}
+					/>
+				))
+			) : <span className='text-base-300/80'>Coming Soon.</span>}
 		</div>
 	);
 }
