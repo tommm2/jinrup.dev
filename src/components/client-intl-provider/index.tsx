5ac1daeb5fed57147c452eb41ac67281ad9345e2
tomjin871114 <--global>
2023-12-06 01:07:49 +0800

    feat: add client-intl-provider

---
+++ src/components/client-intl-provider/index.tsx
@@ -0,0 +1,28 @@
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+
+import { pick } from '@/utils/helpers';
+
+type ClientIntlProviderProps = {
+	messageKey: string;
+	children: React.ReactNode;
+}
+
+function ClientIntlProvider({
+	messageKey,
+	children,
+}: ClientIntlProviderProps) {
+	const messages = useMessages();
+
+	return (
+		<NextIntlClientProvider
+			messages={
+				// Only provide the minimum of messages
+				pick(messages, messageKey)
+			}
+		>
+			{children}
+		</NextIntlClientProvider>
+	);
+}
+
+export default ClientIntlProvider;
