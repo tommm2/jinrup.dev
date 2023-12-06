4d72ef0016b4e15b558d556a675397e15770b97d
tomjin871114 <--global>
2023-12-06 13:23:26 +0800

    fix: update font configs

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -2,14 +2,16 @@ import { Inter, Noto_Sans_TC } from 'next/font/google';
 
 export const fontSans = Inter({
 	variable: '--font-sans',
-	display: 'swap',
+	adjustFontFallback: true,
+	display: 'optional',
 	subsets: ['latin'],
 	weight: ['400', '500', '700'],
 });
 
 export const fontNoto = Noto_Sans_TC({
 	variable: '--font-noto',
-	display: 'swap',
+	adjustFontFallback: true,
+	display: 'optional',
 	subsets: ['latin'],
 	weight: ['400', '500', '700'],
 });
