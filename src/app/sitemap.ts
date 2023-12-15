b76779c5fbe2e8f551fdbbbf888127ef27796cb2
tomjin871114 <--global>
2023-12-15 23:17:32 +0800

    fix: add projects in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,7 +8,7 @@ export default async function sitemap() {
 		lastModified: post.publishedAt.split('T')[0],
 	}));
 
-	const routes = ['', 'about', 'blog']
+	const routes = ['', 'about', 'blog', 'projects']
 		.map((route) => ({
 			url: `${siteConfig.siteUrl}/${route}`,
 			lastModified: new Date().toISOString().split('T')[0],
