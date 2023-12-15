1865bcf1df99122c9dafad9a04d5bb2b8a4b6168
tomjin871114 <--global>
2023-12-15 23:14:59 +0800

    fix: remove PinnedRepo type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,23 +5,6 @@ type Views = {
 	views: number;
 }
 
-type PinnedRepo = {
-	id: any;
-	name?: string;
-	url?: string;
-	stargazerCount?: number;
-	primaryLanguage?: {
-		name: string;
-		color: string;
-		id: string;
-	};
-	description?: string;
-	createdAt?: string;
-	forkCount?: number;
-	isArchived?: boolean;
-	isFork?: boolean;
-}
-
 type Stats = {
 	followers: number;
 	stars: number;
