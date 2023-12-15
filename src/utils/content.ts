a06624cb9f9adac03a3347f855e00d14f1adfe0f
tomjin871114 <--global>
2023-12-15 23:14:36 +0800

    feat: add content util

---
+++ src/utils/content.ts
@@ -0,0 +1,23 @@
+import type { Post, Project } from 'contentlayer/generated';
+import { defaultLocale } from '@/lib/navigation';
+
+export function getContentBySlugAndLocale<T extends Post | Project>({
+	contentItems,
+	slug,
+	locale,
+}: {
+	contentItems: T[];
+	slug: string;
+	locale: Locale;
+}): T | undefined {
+	const itemsWithMatchingSlug = contentItems.filter((item) => item.slug === slug);
+	const index = itemsWithMatchingSlug.findIndex((item) => item.language === locale);
+
+	if (index === -1) {
+		const defaultItem = itemsWithMatchingSlug.find(item => item.language === defaultLocale);
+
+		return defaultItem;
+	}
+
+	return itemsWithMatchingSlug[index];
+}
