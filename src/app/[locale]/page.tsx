cd62c9c815af04f005b9060d5f56a9a52728b4e3
tomjin871114 <--global>
2023-12-19 22:57:01 +0800

    fix: update home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import {
+	RiArrowRightFill,
+	RiGitRepositoryFill,
 	RiGithubFill,
+	RiHammerFill,
 	RiLinkedinBoxFill,
 	RiMailSendFill,
 } from 'react-icons/ri';
 
 import Link from '@/components/link';
 import { siteConfig } from '@/config/site';
+import { allProjects } from 'contentlayer/generated';
+import ProjectCard from '@/components/project-card';
 
 function HomePage() {
 	const t = useTranslations();
+	const locale = useLocale();
+	const projects = allProjects.filter(item => item.language === locale);
 
 	return (
 		<>
-			<div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
+			<div className='flex items-center gap-4'>
 				<Image
 					className='animate-in rounded-full bg-base-200'
 					src='/avatar.png'
@@ -27,7 +34,14 @@ function HomePage() {
 					<h1 className='text-2xl font-bold tracking-tight text-base-200'>
 						Tom Jin
 					</h1>
-					<p className='text-base-200/70'>{t('homePage.subTitle')}</p>
+					<div className='w-fit rounded-full bg-gradient-to-r from-primary-400 to-accent-400 p-px'>
+						<div className='rounded-full bg-base-950/80 text-sm'>
+							<span className='flex items-center gap-0.5 bg-gradient-to-r from-primary-400 to-accent-400 bg-clip-text px-2 py-px text-sm font-medium text-transparent'>
+								<RiHammerFill className='fill-primary-400' />
+								{t('homePage.subTitle')}
+							</span>
+						</div>
+					</div>
 				</div>
 			</div>
 			<p className='mt-8 animate-in animation-delay-2'>
@@ -62,10 +76,24 @@ function HomePage() {
 					<span className='hidden sm:inline'>Email</span>
 				</Link>
 			</div>
-			<div className='mt-16 animate-in space-y-4 animation-delay-4'>
-				<h2 className='flex items-center gap-2 tracking-tight text-base-200'>
-					{t('common.pinned')}
-				</h2>
+			<div className='mt-16 animate-in animation-delay-4'>
+				<div className='flex items-center justify-between'>
+					<h2 className='flex items-center gap-1 text-lg font-medium  tracking-tight text-base-200'>
+						<RiGitRepositoryFill />
+						{t('common.projects')}
+					</h2>
+					<Link
+						className='opacity-hover flex items-center gap-1 text-sm '
+						href='/projects'
+						aria-label={t('common.projects')}
+					>
+						See More
+						<RiArrowRightFill />
+					</Link>
+				</div>
+				<div className='mt-2 grid grid-cols-1 gap-2 sm:grid-cols-2'>
+					{projects.map(project => <ProjectCard key={project.slug} project={project} />)}
+				</div>
 			</div>
 		</>
 	);
