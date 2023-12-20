dfad4bdac0477c2686939ca0143fd9baa689a281
tomjin871114 <--global>
2023-12-20 23:04:19 +0800

    fix: update homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,93 +1,44 @@
-import Image from 'next/image';
 import { useLocale, useTranslations } from 'next-intl';
-import {
-	RiArrowRightFill,
-	RiGitRepositoryFill,
-	RiGithubFill,
-	RiHammerFill,
-	RiLinkedinBoxFill,
-	RiMailSendFill,
-} from 'react-icons/ri';
+import { RiArrowRightSLine } from 'react-icons/ri';
+import { allProjects } from 'contentlayer/generated';
 
 import Link from '@/components/link';
-import { siteConfig } from '@/config/site';
-import { allProjects } from 'contentlayer/generated';
 import ProjectCard from '@/components/project-card';
+import Avatar from '@/components/avatar';
+import Contacts from '@/components/contacts';
+import GradientText from '@/components/gradient-text';
 
 function HomePage() {
 	const t = useTranslations();
 	const locale = useLocale();
-	const projects = allProjects.filter(item => item.language === locale);
+	const projects = allProjects
+		.filter(item => item.language === locale)
+		.splice(0, 2);
 
 	return (
 		<>
-			<div className='flex items-center gap-4'>
-				<Image
-					className='animate-in rounded-full bg-base-200'
-					src='/avatar.png'
-					alt='avatar'
-					width={80}
-					height={80}
-					priority
-				/>
-				<div className='animate-in animation-delay-1'>
-					<h1 className='text-2xl font-bold tracking-tight text-base-200'>
-						Tom Jin
-					</h1>
-					<div className='w-fit rounded-full bg-gradient-to-r from-primary-400 to-accent-400 p-px'>
-						<div className='rounded-full bg-base-950/80 text-sm'>
-							<span className='flex items-center gap-0.5 bg-gradient-to-r from-primary-400 to-accent-400 bg-clip-text px-2 py-px text-sm font-medium text-transparent'>
-								<RiHammerFill className='fill-primary-400' />
-								{t('homePage.subTitle')}
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<p className='mt-8 animate-in animation-delay-2'>
+			<Avatar
+				title='Tom Jin'
+				subTitle={t('homePage.subTitle')}
+			/>
+			<p className='mt-8 animate-in animation-delay-3'>
 				{t('homePage.description')}
 			</p>
-			<div className='mt-4 flex animate-in flex-wrap items-center gap-4 animation-delay-3'>
-				<Link
-					className='opacity-hover inline-flex items-center gap-1 font-medium text-base-300/80'
-					aria-label='GitHub'
-					title='GitHub'
-					href={siteConfig.links.github}
-				>
-					<RiGithubFill className='h-5 w-5' />
-					<span className='hidden sm:inline'>Github</span>
-				</Link>
-				<Link
-					className='opacity-hover inline-flex items-center gap-1 font-medium text-blue-500'
-					aria-label='Linkedin'
-					title='Linkedin'
-					href={siteConfig.links.linkedin}
-				>
-					<RiLinkedinBoxFill className='h-5 w-5' />
-					<span className='hidden sm:inline'>Linkedin</span>
-				</Link>
-				<Link
-					className='opacity-hover inline-flex items-center gap-1 font-medium text-rose-500'
-					aria-label='Email'
-					title='Email'
-					href={`mailto:mail@${siteConfig.email}`}
-				>
-					<RiMailSendFill className='h-5 w-5' />
-					<span className='hidden sm:inline'>Email</span>
-				</Link>
-			</div>
-			<div className='mt-16 animate-in animation-delay-4'>
+			<Contacts className='mt-4 animate-in animation-delay-4' />
+			<div className='mt-16 animate-in animation-delay-5'>
 				<div className='flex items-center justify-between'>
-					<h2 className='flex items-center gap-1 text-lg font-medium  tracking-tight text-base-200'>
-						<RiGitRepositoryFill />
+					<GradientText
+						as='h2'
+						className='text-xl font-bold tracking-tight'
+					>
 						{t('common.projects')}
-					</h2>
+					</GradientText>
 					<Link
-						className='opacity-hover flex items-center gap-1 text-sm '
+						className='flex h-8 items-center gap-1 rounded-lg bg-base-400/10 p-4 text-sm text-base-300 transition-colors hover:bg-base-400/20'
 						href='/projects'
 					>
 						{t('common.viewMore')}
-						<RiArrowRightFill />
+						<RiArrowRightSLine className='h-4 w-4' />
 					</Link>
 				</div>
 				<div className='mt-2 grid grid-cols-1 gap-2 sm:grid-cols-2'>
