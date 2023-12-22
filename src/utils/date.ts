de13e47b6176c4ac79c6dfb4ac8f759f772057ac
tomjin871114 <--global>
2023-12-22 22:09:28 +0800

    fix: update date util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
 import { format, formatDistanceToNow, parseISO } from 'date-fns';
-import { zhTW } from 'date-fns/locale';
+import { zhTW, enUS } from 'date-fns/locale';
 
 import { defaultLocale } from '@/lib/navigation';
 
@@ -15,13 +15,13 @@ export function formatDate({
 	locale,
 }: formatDateType) {
 	return format(parseISO(date), formatString, {
-		locale: locale === defaultLocale ? zhTW : undefined,
+		locale: locale === defaultLocale ? zhTW : enUS,
 	});
 }
 
 export function getDistanceToNow(date: string, locale: Locale) {
 	return formatDistanceToNow(new Date(date), {
-		locale: locale === defaultLocale ? zhTW : undefined,
+		locale: locale === defaultLocale ? zhTW : enUS,
 		addSuffix: true,
 	},);
 }
