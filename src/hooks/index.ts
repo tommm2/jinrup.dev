2e728930b115e41cac33c14fb6c49afdf2a3a948
tomjin871114 <--global>
2023-12-22 17:57:10 +0800

    chore: remove unuse hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,7 @@
 import useEnabledFirstInView from './use-enabled-first-view';
-import usePollIfInView from './use-poll-in-view';
 import usePostViews from './use-post-views';
 
 export {
 	useEnabledFirstInView,
-	usePollIfInView,
 	usePostViews,
 };
