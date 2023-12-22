5b56272057e995d0df6ab24bfe96ca36992c0a67
tomjin871114 <--global>
2023-12-22 23:04:50 +0800

    fix: add repoName field in project doc type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -44,6 +44,7 @@ export const Project = defineDocumentType(() => ({
 		imageUrl: { type: 'string', required: true },
 		demoUrl: { type: 'string', required: true },
 		repoUrl: { type: 'string', required: true },
+		repoName: { type: 'string', required: true },
 		language: { type: 'string', required: true },
 		tags: { type: 'list', of: { type: 'string' } },
 	},
