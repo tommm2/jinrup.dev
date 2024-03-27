9694c9f9f8951eb71f938f7f5656eecb5b93855c
tomjin871114 <tom875694231@gmail.com>
2024-03-27 18:51:57 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	swcMinify: true,
+	output: 'export',
 };
 
 module.exports = withNextIntl(nextConfig);
