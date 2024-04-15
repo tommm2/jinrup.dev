65be9c46641ba2ca4f0b331c3036e7d5b99ff338
tomjin871114 <tom875694231@gmail.com>
2024-04-15 09:52:31 +0800

    feat: add linked-heading

---
+++ src/components/mdx/linked-heading.tsx
@@ -0,0 +1,45 @@
+import Link from '@/components/ui/link';
+import { cn } from '@/utils/cn';
+
+type LinkedHeadingProps = {
+	className?: string;
+	as: keyof JSX.IntrinsicElements;
+	id?: string;
+	children?: React.ReactNode;
+};
+
+const anchorEncode = (text?: string) => {
+	if (!text) return undefined;
+
+	return text.toLowerCase().replace(/ /g, '-');
+};
+
+const LinkedHeading = ({
+	className,
+	as,
+	id: idProp,
+	children,
+	...otherProps
+}: LinkedHeadingProps) => {
+	const Component = as;
+
+	let id = idProp || anchorEncode(children as string);
+
+	return (
+		<Component
+			id={id}
+			className={cn('mb-[0.5em] w-fit mt-[1em] scroll-mt-20 group', className)}
+			{...otherProps}
+		>
+			<Link
+				className='anchor bg-gradient-to-b from-foreground from-30% to-foreground/40 bg-clip-text font-bold tracking-tight text-transparent no-underline [&_>_svg]:opacity-0 group-hover:[&_>_svg]:opacity-100'
+				href={`#${id}`}
+				showAnchorIcon
+			>
+				{children}
+			</Link>
+		</Component>
+	);
+};
+
+export default LinkedHeading;
