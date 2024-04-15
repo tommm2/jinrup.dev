8023e9b742f81732709fc170bdc9b0194c8d3bf9
tomjin871114 <tom875694231@gmail.com>
2024-04-15 22:42:26 +0800

    fix: add layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import '@/styles/app.css';
+
+
+type RootLayoutProps = {
+	children: React.ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+	return children;
+};
+
+export default RootLayout;
