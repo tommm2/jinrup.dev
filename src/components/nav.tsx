b794c1e9ec11bcae39083db638329777e46c4ba2
tomjin871114 <tom875694231@gmail.com>
2024-05-28 01:56:52 +0800

    fix: update nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,22 +1,41 @@
+'use client';
+
 import { useTranslations } from 'next-intl';
 
 import { navLinks } from '@/config/nav-links';
+import { usePathname } from '@/lib/navigation';
+import { cn } from '@/utils/cn';
 
-import ActiveLink from './active-link';
+import Link from './ui/link';
 
 const Nav = () => {
 	const t = useTranslations('common');
+	const pathname = usePathname();
 
 	return (
 		<nav>
 			<ul className='hidden gap-1 md:flex'>
-				{navLinks.map((item) => (
-					<ActiveLink
-						key={item.title}
-						title={t(item.title)}
-						href={item.href}
-					/>
-				))}
+				{navLinks.map((item) => {
+					const isActive = pathname.includes(item.href);
+
+					return (
+						<li key={item.title}>
+							<Link
+								className='group relative flex items-center rounded py-1.5 pl-5 pr-2 text-sm transition-colors hover:bg-accent/60'
+								href={item.href}
+							>
+								<span
+									className={cn(
+										'absolute duration-200 left-2 transition-all flex-none rounded-full bg-primary group-hover:size-1.5',
+										{ 'size-1.5': isActive },
+										{ 'size-0': !isActive },
+									)}
+								/>
+								<span>{t(item.title)}</span>
+							</Link>
+						</li>
+					);
+				})}
 			</ul>
 		</nav>
 	);
