68ef74244853a598189f09e9d83db306823cbc93
tomjin871114 <tom875694231@gmail.com>
2024-06-12 22:13:35 +0800

    chore: remove jest.config.js comment

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
 	dir: './',
 });
 
-// Add any custom config to be passed to Jest
-const customJestConfig = {
+const config = {
 	coverageProvider: 'v8',
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(config);
