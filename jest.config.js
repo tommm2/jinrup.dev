1949ed0cb45ddbd9d8dfc7799a58b3e90e5c9e84
tomjin871114 <tom875694231@gmail.com>
2024-06-14 22:20:59 +0800

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ const config = {
 	coverageProvider: 'v8',
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleNameMapper: {
+		'next-intl': '<rootDir>/src/components/__mocks__/next-intl.ts',
+	},
 };
 
 module.exports = createJestConfig(config);
