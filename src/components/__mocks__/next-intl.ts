54fefa218d3009402a9da485bfbc4a9f60bf185c
tomjin871114 <tom875694231@gmail.com>
2024-06-15 17:57:01 +0800

    update: next-intl mocks

--- src/components/__mocks__/next-intl.ts
+++ src/components/__mocks__/next-intl.ts
@@ -1,3 +1,4 @@
+import { type DateTimeFormatOptions } from 'next-intl';
 import Link from 'next/link';
 import { redirect, usePathname, useRouter } from 'next/navigation';
 
@@ -9,6 +10,15 @@ export const useTimeZone = () => 'Asia/Taipei';
 
 export const useLocale = () => 'zh-TW';
 
+export const useFormatter = () => {
+	const formatter = (format: DateTimeFormatOptions) => new Intl.DateTimeFormat('zh-TW', format);
+
+	return {
+		date: (date: Date, format: DateTimeFormatOptions) => formatter(format).format(date),
+		dateTime: (date: Date, format: DateTimeFormatOptions) => formatter(format).format(date),
+	};
+};
+
 export const useTranslations = () => {
 	const t = (key: string) => key;
 
@@ -18,7 +28,11 @@ export const useTranslations = () => {
 	return t;
 };
 
-export const NextIntlClientProvider = ({ children }: { children: React.ReactNode }) => children;
+export const NextIntlClientProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => children;
 
 export const createSharedPathnamesNavigation = () => ({
 	Link: Link,
