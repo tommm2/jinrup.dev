85dd547cdf2912ef88e89686e22f146c8a802594
tomjin871114 <tom875694231@gmail.com>
2024-06-15 18:15:34 +0800

    fix: remove compareDesc

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,3 @@
-import { compareDesc } from 'date-fns';
 import { Metadata } from 'next';
 import { useLocale, useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
@@ -33,9 +32,7 @@ const BlogPage = () => {
 	const locale = useLocale();
 	const posts = allPosts
 		.filter((post) => post.language === locale)
-		.sort((a, b) =>
-			compareDesc(new Date(a.publishedAt), new Date(b.publishedAt)),
-		);
+		.sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime());
 
 	return (
 		<>
