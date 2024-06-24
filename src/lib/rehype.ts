b64dbd3bdd7ddfa65067596c2d1fb892661f9192
tomjin871114 <tom875694231@gmail.com>
2024-06-24 20:07:15 +0800

    chore: remove rehype lib

--- src/lib/rehype.ts
+++
@@ -1,9 +0,0 @@
-import rehypePrettyCode, { type Options } from 'rehype-pretty-code';
-
-export const prettyCode: [typeof rehypePrettyCode, Options] = [
-	rehypePrettyCode,
-	{
-		keepBackground: false,
-		theme: 'github-dark',
-	},
-];
