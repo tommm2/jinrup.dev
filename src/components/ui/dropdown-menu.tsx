9d7eb35e9ab0322aaed66a4297cc976c2b29ad39
tomjin871114 <tom875694231@gmail.com>
2024-06-30 21:42:22 +0800

    styl: dropdown-menu

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -16,14 +16,14 @@ const DropdownMenuContent = React.forwardRef<
 		<DropdownMenuPrimitive.Content
 			ref={ref}
 			className={cn(
-				'min-w-44 overflow-hidden rounded-lg border bg-popover shadow-md p-1 text-popover-foreground',
+				'min-w-28 overflow-hidden rounded-lg border bg-popover shadow-md p-1 text-popover-foreground',
 				'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
 				'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
 				'data-[side=top]:slide-in-from-bottom-2 data-[side=bottom]:slide-in-from-top-2',
 				'data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2',
 				className,
 			)}
-			sideOffset={30}
+			sideOffset={20}
 			{...props}
 		/>
 	</DropdownMenuPrimitive.Portal>
