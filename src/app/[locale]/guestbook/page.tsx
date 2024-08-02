2ab962c15d163e6a0d2e35644333232c5da03e02
tomjin871114 <--global>
2024-08-02 22:35:46 +0800

    chore: update guestbook

--- src/app/[locale]/guestbook/page.tsx
+++ src/app/[locale]/guestbook/page.tsx
@@ -1,30 +1,38 @@
 import { signIn, signOut , auth } from '@/lib/auth';
+import Image from 'next/image';
 
 const GuestbookPage = async () => {
 	const session = await auth();
 
 	if (!session?.user) {
 		return (
-			<form
-				action={async () => {
-					'use server';
-					await signIn('github');
-				}}
-			>
-				<button type='submit'>Signin with GitHub</button>
-			</form>
+			<>
+				<form
+					action={async () => {
+						'use server';
+						await signIn('github');
+					}}
+				>
+					<button type='submit'>Signin with GitHub</button>
+				</form>
+			</>
 		);
 	}
 
 	return (
-		<form
-			action={async () => {
-				'use server';
-				await signOut();
-			}}
-		>
-			<button type='submit'>Signout with GitHub</button>
-		</form>
+		<>
+			<h1>Welcome {session.user.name}</h1>
+			<Image width={60} height={60} alt='avatar' src={session.user.image as string} />
+			<form
+				action={async () => {
+					'use server';
+					await signOut();
+				}}
+			>
+				<button type='submit'>Signout with GitHub</button>
+			</form>
+			<input type="text" placeholder='leave you message' />
+		</>
 	);
 };
 
